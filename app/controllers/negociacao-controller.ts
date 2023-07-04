74c63eaaae320a7930d575cd130b1032b808c50f
Wagner Oliveira <contato-wagneros@outlook.com>
2023-07-04 17:35:56 -0300

    Feature
    Incluir lista sem poder exclui-las ou altera-las

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js"
+import { Negociacoes } from "../models/negociacoes.js"
 
 export class NegociacaoController {
     private inputData: HTMLInputElement
     private inputQuantidade: HTMLInputElement
     private inputValor: HTMLInputElement
+    private negociacoes: Negociacoes = new Negociacoes()
 
     constructor(){
         this.inputData = document.querySelector('#data')
@@ -13,6 +15,10 @@ export class NegociacaoController {
 
     adiciona(): void{
         const negociacao = this.criaNegociacao()
+        this.negociacoes.adiciona(negociacao)
+        const negociacoes = this.negociacoes.lista()
+        console.log(negociacoes)
+        this.limparFormulario()
     }
 
     criaNegociacao(): Negociacao{
@@ -21,5 +27,12 @@ export class NegociacaoController {
         const quantidade = parseInt(this.inputQuantidade.value)
         const valor = parseFloat(this.inputValor.value)
         return new Negociacao(date, quantidade, valor)
+    }
+
+    limparFormulario(): void{
+        this.inputData.value = ''
+        this.inputQuantidade.value = ''
+        this.inputValor.value = ''
+        this.inputData.focus()
     }
 }
