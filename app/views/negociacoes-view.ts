8fd9bbe04a332c1c6290fb62f514150307cbafd0
Wagner Oliveira <contato-wagneros@outlook.com>
2023-07-06 16:30:15 -0300

    Template

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,5 +1,13 @@
+import { Negociacoes } from "../models/negociacoes.js"
+
 export class NegociacoesView{
-    template(): string{
+    
+    private elemento: HTMLElement
+    constructor(seletor: string){
+        this.elemento = document.querySelector(seletor)
+    }
+
+    template(model: Negociacoes): string{
         return `
             <table class="table table-hover table-bordered">
                 <thead>
@@ -10,12 +18,22 @@ export class NegociacoesView{
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                        </td>
-                    </tr>
+                    ${model.lista().map(negociacao =>{
+                        return `
+                            <tr>
+                                <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                                <td>${negociacao.quantidade}</td>
+                                <td>${negociacao.valor}</td>
+                            </tr>
+                        `
+                    }).join('')}
                 </tbody>
             </table>
         `
+    }
+    update(model: Negociacoes): void{
+        const template = this.template(model)
+        console.log(template)
+        this.elemento.innerHTML = template
     }
 }
